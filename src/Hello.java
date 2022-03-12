b0ffddb7595b8a9e53f1be2305aa68dd7c04f75f
Julian Dave Shikui <juliandave1989@gmail.com>
2022-03-12 22:00:37 +0200

    bytes example

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,14 @@
 public class Hello {
     public static void main(String[] args) {
 
-        int myMinIntValue = Integer.MIN_VALUE;
-        int myMaxIntValue = Integer.MAX_VALUE;
-        System.out.println("Integer maximum value = " + myMaxIntValue);
-        System.out.println("Integer minimum value = " + myMinIntValue);
-        System.out.println("Busted max value = " + (myMaxIntValue + 1)); // overflow;
-        System.out.println("Busted min value = " + (myMinIntValue - 1)); // underflow;
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE ;
+        System.out.println("Byte maximum value = " + myMaxByteValue);
+        System.out.println("Byte minimum value = " + myMinByteValue);
+        
+        // A Byte automatically gets converted to an int by the compiler if its out of range.
+        System.out.println("Busted max value = " + (myMaxByteValue + 1111)); // overflow;
+        System.out.println("Busted min value = " + (myMinByteValue - Integer.MIN_VALUE - 124)); // underflow;
 
     }
 }
