a0adc4cd76bbbb2016f5c292e7c0c5a54cba44e8
Julian Dave Shikui <juliandave1989@gmail.com>
2022-03-12 21:42:33 +0200

    (over/under)flow example

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
-        
-        var helloWorld = "Hello World";
-        System.out.println(helloWorld);
-        int myFirstNumber = 5;
-        System.out.println(myFirstNumber);
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer maximum value = " + myMaxIntValue);
+        System.out.println("Integer minimum value = " + myMinIntValue);
+        System.out.println("Busted max value = " + (myMaxIntValue + 1)); // overflow;
+        System.out.println("Busted min value = " + (myMinIntValue - 1)); // underflow;
+
     }
 }
