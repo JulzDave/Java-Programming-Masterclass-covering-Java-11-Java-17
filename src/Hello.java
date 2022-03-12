b43433a7db625ef2ffca4f328ce881889b3d1eec
Julian Dave Shikui <juliandave1989@gmail.com>
2022-03-12 22:45:14 +0200

    Added comment

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
-
         long myMinLongValue = Long.MIN_VALUE;
         long myMaxLongValue = Long.MAX_VALUE;
         System.out.println("Long maximum value = " + myMaxLongValue);
         System.out.println("Long minimum value = " + myMinLongValue);
         System.out.println("Busted max value = " + (myMaxLongValue + 1)); // overflow;
         System.out.println("Busted min value = " + (myMinLongValue - 1)); // underflow;
-
+        // long number literals should contain the suffix 'L' 
     }
 }
