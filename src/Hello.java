fad22c2138006f18dfc3416b2edc3c4c4c73e537
Julian Dave Shikui <juliandave1989@gmail.com>
2022-03-12 21:19:02 +0200

    finished lesson 24

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        var helloWorld = "Hello World";
+        System.out.println(helloWorld);
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+    }
+}
