bf71e3d9a92c9fca7fa21bc68d02554abef053b0
Julian Dave Shikui <juliandave1989@gmail.com>
2022-03-12 23:18:15 +0200

    my answer

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,12 @@
 public class Hello {
-    public static void main(String[] args) {
 
-        byte myMinByteValue = Byte.MIN_VALUE;
-        byte myNewByteValue = (byte) (myMinByteValue / 2); 
-        System.out.println(myNewByteValue);
-    }
+  public static void main(String[] args) {
+    //prettier-ignore
+    byte  myByteValue  = 50;
+    short myShortValue = 500_0;
+    int   myIntValue   = 500_000;
+    long  myLongValue  = 50_000 + (10 * (myByteValue + myShortValue + myIntValue));
+    
+    System.out.println(myLongValue);
+  }
 }
