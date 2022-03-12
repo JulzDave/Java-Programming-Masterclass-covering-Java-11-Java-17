56b1d4dc38af9695d100172f315f9a3e25f705b1
Julian Dave Shikui <juliandave1989@gmail.com>
2022-03-12 22:13:08 +0200

    long example

--- src/Hello.java
+++ src/Hello.java
@@ -1,14 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
 
-        short myMinShortValue = Short.MIN_VALUE;
-        short myMaxShortValue = Short.MAX_VALUE ;
-        System.out.println("Short maximum value = " + myMaxShortValue);
-        System.out.println("Short minimum value = " + myMinShortValue);
-        
-        // A Short automatically gets converted to an int by the compiler if its out of range.
-        System.out.println("Busted max value = " + (myMaxShortValue + 1111)); // overflow;
-        System.out.println("Busted min value = " + (myMinShortValue - Integer.MIN_VALUE - 124)); // underflow;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long maximum value = " + myMaxLongValue);
+        System.out.println("Long minimum value = " + myMinLongValue);
+        System.out.println("Busted max value = " + (myMaxLongValue + 1)); // overflow;
+        System.out.println("Busted min value = " + (myMinLongValue - 1)); // underflow;
 
     }
 }
